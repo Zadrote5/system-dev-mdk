55fc8b6137c98a78ef013b331896df5dee465833
Артем <tikhonovich.a@verweb.ru>
2021-11-06 12:19:13 +0200

    third work

---
+++ structurs/cmake-build-debug/CMakeFiles/structurs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/structurs.dir/main.cpp.o"
+  "structurs"
+  "structurs.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/structurs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
